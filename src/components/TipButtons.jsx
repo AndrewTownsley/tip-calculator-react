36ba5495f854cc8f6780fd38eb0f07251e81c6a3
Andrew Townsley <atownsley86@gmail.com>
2022-02-07 21:32:27 -0700

    Updated 20% button to calculate proper tip amount.

--- src/components/TipButtons.jsx
+++ src/components/TipButtons.jsx
@@ -8,7 +8,7 @@ const TipButtons = ({ setPercent }) => {
 
                 <button onClick={(e) => setPercent(.15)} className="tip-btn">15%</button>
                 <button onClick={(e) => setPercent(.18)} className="tip-btn">18%</button>
-                <button onClick={(e) => setPercent(.10)} className="tip-btn">20%</button>
+                <button onClick={(e) => setPercent(.20)} className="tip-btn">20%</button>
                 <button onClick={(e) => setPercent(.25)} className="tip-btn">25%</button>
                 <button onClick={(e) => setPercent(.30)} className="tip-btn">30%</button>
                 <input onChange={(e) => setPercent(e.target.value * .01)} type="number" placeholder="Custom %" />
