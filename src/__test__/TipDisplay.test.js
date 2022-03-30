fe5b3a68136936c81a399fe2ea3d1bfc9e303753
Andrew TOwnsley <atownsley86@gmail.com>
2022-03-30 17:56:42 -0600

    Continuing to write tests.

--- src/__test__/TipDisplay.test.js
+++ src/__test__/TipDisplay.test.js
@@ -6,7 +6,7 @@ import '@testing-library/jest-dom/extend-expect';
 import TipDisplay from "../components/TipDisplay";
 
 
-it('renders without crashing', () => {
+it('Component renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<TipDisplay/>, div);
     ReactDOM.unmountComponentAtNode(div)
